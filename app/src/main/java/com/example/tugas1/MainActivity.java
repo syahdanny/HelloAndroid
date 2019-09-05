0e01fdc983bd1944f8bafe8f4e6ee3d89b25e272
syahdanny <syahdanny11@gmail.com>
2019-09-05 14:58:04 +0700

    menambahkan simple activity

--- app/src/main/java/com/example/tugas1/MainActivity.java
+++ app/src/main/java/com/example/tugas1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tugas1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "On Start", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "On Stop", Toast.LENGTH_SHORT).show();
     }
 }
